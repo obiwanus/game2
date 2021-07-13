91313ba3f7e36da4533c77ba7480d340f8e83775
Ivan Ivanov <ivan@ivanovs.info>
2021-07-13 18:26:43 +1200

    Open a blank window

---
+++ src/camera.rs
@@ -0,0 +1,140 @@
+#![allow(dead_code)]
+
+use std::f32::consts::PI;
+
+use glam::{Mat4, Vec3};
+
+use super::utils::clamp;
+
+const FOV_MIN: f32 = 0.01 * PI;
+const FOV_MAX: f32 = 0.5 * PI;
+
+const ZOOM_MIN: f32 = 1.0;
+const ZOOM_MAX: f32 = 100.0;
+const ZOOM_DEFAULT: f32 = 30.0;
+
+const PITCH_MIN: f32 = -0.49 * PI;
+const PITCH_MAX: f32 = 0.49 * PI;
+
+pub enum Movement {
+    Forward,
+    Backward,
+    Left,
+    Right,
+}
+
+#[derive(Debug)]
+pub struct Camera {
+    pub position: Vec3,
+    direction: Vec3,
+    up: Vec3,
+    right: Vec3,
+
+    yaw: f32,
+    pitch: f32,
+
+    movement_speed: f32,
+    sensitivity: f32,
+    zoom: f32,
+    pub aspect_ratio: f32,
+    locked: bool, // whether to allow flying
+    pub moved: bool,
+}
+
+impl Camera {
+    pub fn new(position: Vec3, look_at: Vec3, aspect_ratio: f32) -> Self {
+        let mut camera = Camera {
+            position,
+            direction: position + look_at,
+            up: Vec3::new(0.0, 1.0, 0.0),
+            right: Vec3::new(1.0, 0.0, 0.0),
+
+            yaw: 0.0,
+            pitch: 0.0,
+
+            movement_speed: 15.0,
+            sensitivity: 0.0015,
+            zoom: ZOOM_DEFAULT,
+            aspect_ratio,
+            locked: false,
+            moved: true,
+        };
+        camera.look_at(look_at);
+        camera
+    }
+
+    /// Point the camera at the target.
+    /// Sets direction, right and Euler angles accordingly
+    pub fn look_at(&mut self, target: Vec3) {
+        self.direction = (target - self.position).normalize();
+        let (x, y, z) = (self.direction.x, self.direction.y, self.direction.z);
+        self.pitch = y.asin();
+        self.pitch = clamp(self.pitch, PITCH_MIN, PITCH_MAX);
+        self.yaw = (z / x).atan();
+        self.right = self.recalculate_right();
+    }
+
+    /// Move the camera
+    pub fn go(&mut self, direction: Movement, delta_time: f32) {
+        let speed = self.movement_speed * delta_time;
+        let projected_direction = if self.locked {
+            Vec3::new(self.direction.x, 0.0, self.direction.z)
+        } else {
+            self.direction
+        };
+        match direction {
+            Movement::Forward => self.position += speed * projected_direction,
+            Movement::Backward => self.position -= speed * projected_direction,
+            Movement::Left => self.position -= speed * self.right,
+            Movement::Right => self.position += speed * self.right,
+        }
+        self.moved = true;
+    }
+
+    /// Zoom is used to calculate the vertical FOV:
+    ///
+    /// 1.0 corresponds to FOV_MAX,
+    /// 100.0 corresponds to FOV_MIN.
+    pub fn adjust_zoom(&mut self, delta: i32) {
+        self.zoom += delta as f32;
+        self.zoom = clamp(self.zoom, ZOOM_MIN, ZOOM_MAX);
+    }
+
+    pub fn rotate(&mut self, yaw_delta: f64, pitch_delta: f64) {
+        // Adjust Euler angles
+        self.pitch -= pitch_delta as f32 * self.sensitivity;
+        self.pitch = clamp(self.pitch, PITCH_MIN, PITCH_MAX);
+        self.yaw += yaw_delta as f32 * self.sensitivity;
+
+        // Recalculate direction
+        self.direction = Vec3::new(
+            self.pitch.cos() * self.yaw.cos(),
+            self.pitch.sin(),
+            self.pitch.cos() * self.yaw.sin(),
+        )
+        .normalize();
+        self.right = self.recalculate_right();
+
+        self.moved = true;
+    }
+
+    fn recalculate_right(&self) -> Vec3 {
+        self.direction.cross(self.up).normalize()
+    }
+
+    /// Calculate vertical FOV based on zoom level
+    pub fn fov(&self) -> f32 {
+        let t = (self.zoom - ZOOM_MIN) / (ZOOM_MAX - ZOOM_MIN);
+        (1.0 - t) * FOV_MAX + t * FOV_MIN
+    }
+
+    pub fn get_view_matrix(&self) -> Mat4 {
+        Mat4::look_at_rh(self.position, self.position + self.direction, self.up)
+    }
+
+    pub fn get_projection_matrix(&self) -> Mat4 {
+        let mut proj = Mat4::perspective_rh(self.fov(), self.aspect_ratio, 0.1, 100.0);
+        proj.y_axis.y *= -1.0; // account for the Vulkan coordinate system
+        proj
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,199 @@
+extern crate gl as opengl;
+
+mod camera;
+mod utils;
+
+use std::error::Error;
+use std::time::Instant;
+
+use glam::Vec3;
+use glutin::event::{
+    DeviceEvent, ElementState, Event, KeyboardInput, MouseButton, VirtualKeyCode, WindowEvent,
+};
+use glutin::event_loop::{ControlFlow, EventLoop};
+use glutin::window::{Fullscreen, WindowBuilder};
+use glutin::{Api, GlProfile, GlRequest};
+use glutin::{PossiblyCurrent, WindowedContext};
+
+use camera::Camera;
+
+// ==================================== Main loop =================================================
+
 fn main() {
-    println!("Hello, world!");
+    let event_loop = EventLoop::new();
+    let mut game = Game::new(&event_loop).unwrap_or_else(|error| {
+        eprintln!("{}", error);
+        std::process::exit(1);
+    });
+
+    event_loop.run(move |event, _, control_flow| {
+        if let Err(error) = game.main_loop(event, control_flow) {
+            eprint!("{}", error);
+            std::process::exit(1);
+        };
+    });
+}
+
+// ==================================== Game ======================================================
+
+#[derive(Default)]
+struct Input {
+    forward: bool,
+    back: bool,
+    left: bool,
+    right: bool,
+}
+
+struct Game {
+    windowed_context: WindowedContext<PossiblyCurrent>,
+    input: Input,
+    camera: Camera,
+    in_focus: bool,
+    frame_start: Instant,
+}
+
+impl Game {
+    /// Creates a window and inits a new game
+    fn new(event_loop: &EventLoop<()>) -> Result<Self, Box<dyn Error>> {
+        // Create window
+        let window_builder = WindowBuilder::new()
+            .with_title("Game 2")
+            .with_resizable(false)
+            .with_fullscreen(Some(Fullscreen::Borderless(event_loop.primary_monitor())));
+        // .with_inner_size(glutin::dpi::LogicalSize::new(1366.0, 768.0));
+
+        let gl_request = GlRequest::Specific(Api::OpenGl, (3, 3));
+        let gl_profile = GlProfile::Core;
+        let windowed_context = glutin::ContextBuilder::new()
+            .with_gl(gl_request)
+            .with_gl_profile(gl_profile)
+            .with_double_buffer(Some(true))
+            .with_depth_buffer(16)
+            .with_vsync(true)
+            .build_windowed(window_builder, &event_loop)?;
+
+        // Set up OpenGL
+        let windowed_context = unsafe { windowed_context.make_current().unwrap() };
+        gl::load_with(|s| windowed_context.get_proc_address(s) as *const _);
+        let window = windowed_context.window();
+        window.set_cursor_grab(true)?;
+        window.set_cursor_visible(false);
+        let window_size = window.inner_size();
+        unsafe {
+            gl::Viewport(0, 0, window_size.width as i32, window_size.height as i32);
+            gl::ClearColor(0.05, 0.05, 0.05, 1.0);
+            gl::Enable(gl::DEPTH_TEST);
+        }
+
+        // Set up camera
+        let camera = Camera::new(
+            Vec3::new(0.0, 10.0, 30.0),
+            Vec3::new(0.0, 0.0, 0.0),
+            window_size.width as f32 / window_size.height as f32,
+        );
+
+        Ok(Game {
+            windowed_context,
+            input: Input::default(),
+            camera,
+            in_focus: true,
+            frame_start: Instant::now(),
+        })
+    }
+
+    fn main_loop(
+        &mut self,
+        event: Event<()>,
+        control_flow: &mut ControlFlow,
+    ) -> Result<(), Box<dyn Error>> {
+        match event {
+            Event::WindowEvent { event, .. } => match event {
+                WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
+                WindowEvent::MouseInput {
+                    button: MouseButton::Left,
+                    state: ElementState::Pressed,
+                    ..
+                } => {
+                    // Mouse button click
+
+                    println!("camera: {:?}", self.camera);
+                }
+                WindowEvent::Focused(focused) => {
+                    self.in_focus = focused;
+                }
+                WindowEvent::KeyboardInput {
+                    input:
+                        KeyboardInput {
+                            state,
+                            virtual_keycode: Some(key),
+                            ..
+                        },
+                    ..
+                } => {
+                    // Key press
+                    match key {
+                        VirtualKeyCode::W => self.input.forward = state == ElementState::Pressed,
+                        VirtualKeyCode::A => self.input.left = state == ElementState::Pressed,
+                        VirtualKeyCode::S => self.input.back = state == ElementState::Pressed,
+                        VirtualKeyCode::D => self.input.right = state == ElementState::Pressed,
+                        VirtualKeyCode::Escape => *control_flow = ControlFlow::Exit,
+                        _ => {}
+                    }
+                }
+                _ => {}
+            },
+            Event::DeviceEvent { event, .. } => match event {
+                DeviceEvent::MouseMotion { delta } if self.in_focus => {
+                    let (yaw_delta, pitch_delta) = delta;
+                    self.camera.rotate(yaw_delta, pitch_delta);
+                }
+                _ => {}
+            },
+            Event::MainEventsCleared => self.update_and_render()?,
+            _ => {}
+        };
+        Ok(())
+    }
+
+    fn update_and_render(&mut self) -> Result<(), Box<dyn Error>> {
+        // Application code
+        let now = Instant::now();
+        let delta_time = now.duration_since(self.frame_start).as_secs_f32();
+        self.frame_start = now;
+
+        // Move camera
+        use camera::Movement::*;
+        if self.input.forward {
+            self.camera.go(Forward, delta_time);
+        }
+        if self.input.left {
+            self.camera.go(Left, delta_time);
+        }
+        if self.input.back {
+            self.camera.go(Backward, delta_time);
+        }
+        if self.input.right {
+            self.camera.go(Right, delta_time);
+        }
+
+        let proj = self.camera.get_projection_matrix();
+        let view = self.camera.get_view_matrix();
+
+        // Draw
+        unsafe {
+            gl::Clear(gl::COLOR_BUFFER_BIT | gl::DEPTH_BUFFER_BIT);
+        }
+
+        self.windowed_context.swap_buffers()?;
+
+        #[cfg(feature = "debug")]
+        {
+            // Display frame time
+            let frame_ms =
+                Instant::now().duration_since(self.frame_start).as_micros() as f32 / 1000.0;
+            println!("frame: {} ms", frame_ms);
+        }
+
+        Ok(())
+    }
 }
