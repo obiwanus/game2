f513ab876a5152173855a1d524fa1dc992bfb204
Ivan Ivanov <ivan@ivanovs.info>
2021-07-19 13:08:18 +1200

    Add our own gl bindings

---
+++ lib/gl/build.rs
@@ -0,0 +1,19 @@
+use gl_generator::{Api, Fallbacks, GlobalGenerator, Profile, Registry};
+use std::env;
+use std::fs::File;
+use std::path::Path;
+
+fn main() {
+    let out_dir = env::var("OUT_DIR").unwrap();
+    let mut file = File::create(&Path::new(&out_dir).join("bindings.rs")).unwrap();
+
+    Registry::new(
+        Api::Gl,
+        (3, 3),
+        Profile::Core,
+        Fallbacks::All,
+        ["GL_NV_command_list"],
+    )
+    .write_bindings(GlobalGenerator, &mut file)
+    .expect("Couldn't generate GL bindings");
+}
