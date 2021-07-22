64557764a28190918a686f118f44ef2ec578e1c3
Ivan Ivanov <ivan@ivanovs.info>
2021-07-22 13:17:50 +1200

    WIP gui more

--- src/editor/gui.rs
+++ src/editor/gui.rs
@@ -1,9 +1,14 @@
+use std::error::Error;
+
 use egui::{CentralPanel, ClippedMesh, CtxRef, RawInput};
 use epaint::Color32;
 use memoffset::offset_of;
 
 use crate::{
-    opengl::buffers::{Buffer, VertexArray},
+    opengl::{
+        buffers::{Buffer, VertexArray},
+        shader::Program,
+    },
     texture::Texture,
 };
 
@@ -15,6 +20,7 @@ pub struct Gui {
     egui_texture: Texture,
     egui_texture_version: Option<u64>,
 
+    shader: Program,
     vao: VertexArray,
     vbo: Buffer,
     ebo: Buffer,
@@ -22,7 +28,7 @@ pub struct Gui {
 
 impl Gui {
     // Note: assuming non-resizable window for now
-    pub fn new(window_width: f32, window_height: f32) -> Gui {
+    pub fn new(window_width: f32, window_height: f32) -> Result<Gui, Box<dyn Error>> {
         let vao = VertexArray::new();
         vao.bind();
         let vbo = Buffer::new();
@@ -84,7 +90,14 @@ impl Gui {
             gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MAG_FILTER, gl::LINEAR as GLint);
         }
 
-        Gui {
+        let shader = Program::new()
+            .vertex_shader("shaders/editor/gui.vert")?
+            .fragment_shader("shaders/editor/gui.frag")?
+            .link()?;
+        shader.set_used();
+        shader.se
+
+        Ok(Gui {
             window_width,
             window_height,
 
@@ -92,10 +105,12 @@ impl Gui {
             egui_texture,
             egui_texture_version: None,
 
+            shader,
+
             vao,
             vbo,
             ebo,
-        }
+        })
     }
 
     pub fn draw(&mut self) {
@@ -181,125 +196,3 @@ struct Vertex {
     uv: [f32; 2],
     srgba: [u8; 4],
 }
-
-// ==================================== TODO =================================================
-
-// [package]
-// name = "egui"
-// version = "0.1.0"
-// edition = "2018"
-
-// # See more keys and their definitions at https://doc.rust-lang.org/cargo/reference/manifest.html
-
-// [dependencies]
-// egui_glium = ""
-// glium = ""
-// egui = ""
-
-// ==================================== example =================================================
-
-// //! Example how to use pure `egui_glium` without [`epi`].
-// use glium::glutin;
-
-// fn create_display(event_loop: &glutin::event_loop::EventLoop<()>) -> glium::Display {
-//     let window_builder = glutin::window::WindowBuilder::new()
-//         .with_resizable(true)
-//         .with_inner_size(glutin::dpi::LogicalSize {
-//             width: 800.0,
-//             height: 600.0,
-//         })
-//         .with_title("egui_glium example");
-
-//     let context_builder = glutin::ContextBuilder::new()
-//         .with_depth_buffer(0)
-//         .with_srgb(true)
-//         .with_stencil_buffer(0)
-//         .with_vsync(true);
-
-//     glium::Display::new(window_builder, context_builder, event_loop).unwrap()
-// }
-
-// fn main() {
-//     let event_loop = glutin::event_loop::EventLoop::with_user_event();
-//     let display = create_display(&event_loop);
-
-//     let mut egui = egui_glium::EguiGlium::new(&display);
-
-//     event_loop.run(move |event, _, control_flow| {
-//         let mut redraw = || {
-//             egui.begin_frame(&display);
-
-//             let mut quit = false;
-
-//             egui::SidePanel::left("my_side_panel").show(egui.ctx(), |ui| {
-//                 ui.heading("Водка водка!");
-//                 if ui.button("Quit").clicked() {
-//                     quit = true;
-//                 }
-
-//                 egui::ComboBox::from_label("Version")
-//                     .width(350.0)
-//                     .selected_text("foo")
-//                     .show_ui(ui, |ui| {
-//                         egui::CollapsingHeader::new("Dev")
-//                             .default_open(true)
-//                             .show(ui, |ui| {
-//                                 ui.label("contents");
-//                             });
-//                     });
-//             });
-
-//             let (needs_repaint, shapes) = egui.end_frame(&display);
-
-//             *control_flow = if quit {
-//                 glutin::event_loop::ControlFlow::Exit
-//             } else if needs_repaint {
-//                 display.gl_window().window().request_redraw();
-//                 glutin::event_loop::ControlFlow::Poll
-//             } else {
-//                 glutin::event_loop::ControlFlow::Wait
-//             };
-
-//             {
-//                 use glium::Surface as _;
-//                 let mut target = display.draw();
-
-//                 let clear_color = egui::Rgba::from_rgb(0.1, 0.3, 0.2);
-//                 target.clear_color(
-//                     clear_color[0],
-//                     clear_color[1],
-//                     clear_color[2],
-//                     clear_color[3],
-//                 );
-
-//                 // draw things behind egui here
-
-//                 egui.paint(&display, &mut target, shapes);
-
-//                 // draw things on top of egui here
-
-//                 target.finish().unwrap();
-//             }
-//         };
-
-//         match event {
-//             // Platform-dependent event handlers to workaround a winit bug
-//             // See: https://github.com/rust-windowing/winit/issues/987
-//             // See: https://github.com/rust-windowing/winit/issues/1619
-//             glutin::event::Event::RedrawEventsCleared if cfg!(windows) => redraw(),
-//             glutin::event::Event::RedrawRequested(_) if !cfg!(windows) => redraw(),
-
-//             glutin::event::Event::WindowEvent { event, .. } => {
-//                 if egui.is_quit_event(&event) {
-//                     *control_flow = glium::glutin::event_loop::ControlFlow::Exit;
-//                 }
-
-//                 egui.on_event(&event);
-
-//                 display.gl_window().window().request_redraw(); // TODO: ask egui if the events warrants a repaint instead
-//             }
-
-//             _ => (),
-//         }
-//     });
-// }
